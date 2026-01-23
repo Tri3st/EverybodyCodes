5d0d08f7ad6132575a11d1fd8f5641ba131b7da8
Martin van Diest <martin.van.diest@gmail.com>
2026-01-23 12:57:27 +0100

    Better toString()

--- exercism/bowling/BowlingGame.java
+++ exercism/bowling/BowlingGame.java
@@ -40,8 +40,21 @@ public class BowlingGame {
     public String toString(){
         StringBuilder rollsLine = new StringBuilder();
         StringBuilder scoresLine = new StringBuilder();
-        for (Frame f: this.frames){
+        for (int i = 0; i < 10; i++){
+            Frame f = this.frames.get(i);
+            Frame nf;
+            if (i == 9) nf = this.extraFrame;
+            else nf = this.frames.get(i + 1);
+            int totalScore = 0;
+            if (f.getType() == FrameType.STRIKE) totalScore = f.getScore() + nf.getStrikeScore();
+            else if (f.getType() == FrameType.SPARE) totalScore = f.getScore() + nf.getSpareScore();
+            else totalScore = f.getScore();
             rollsLine.append(f.getDisplayRoll1()).append(" : ").append(f.getDisplayRoll2()).append(" | ");
+            String spacingStr = "" + totalScore;
+            int spacing = 6 - spacingStr.length();
+            String spac = "";
+            for (int c = 0; c < spacing; c++) spac += " ";
+            scoresLine.append(totalScore).append(spac).append("| ");
         }
         return rollsLine + System.lineSeparator() + scoresLine;
     }
