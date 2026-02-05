17671b2ba5434cdc825c60feec6fee461d6890c5
Martin <57150892+Tri3st@users.noreply.github.com>
2026-02-05 07:03:51 +0000

    Main.java

--- Main.java
+++ Main.java
@@ -12,25 +12,8 @@ class Main {
         int[] test4 = {7,3,4,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
         int[] test5 = {6,4,7,3,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
-        BowlingGame[] bgs = new BowlingGame[5];
-        for (int x = 0; x < 5; x++) bgs[x] = new BowlingGame();
+       
 
-        for (int i = 0; i < 5; i++){
-            BowlingGame bg = new BowlingGame();
-            int[] tests = switch (i) {
-                case 0 -> test1;
-                case 1 -> test2;
-                case 2 -> test3;
-                case 3 -> test4;
-                case 4 -> test5;
-                default -> new int[10];
-            };
-            for (int d: tests){
-                bgs[i].roll(d);
-            }
-            System.out.println(bgs[i]);
-            System.out.println(bgs[i].score());
-        }
 
 
     }
