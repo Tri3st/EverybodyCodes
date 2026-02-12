af1add7820366fbb7498a2c6a2de0bfd14c70a98
Martin van Diest <martin.van.diest@gmail.com>
2026-02-12 12:26:50 +0100

    Finish part 2
    Make some changes in Part

--- algoritmia/quest2/Part.java
+++ algoritmia/quest2/Part.java
@@ -2,25 +2,22 @@ package algoritmia.quest2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Part implements iPart {
     private ArrayList<String> words;
-    private String inscription;
     private ArrayList<String> inscriptions;
-    private ArrayList<String> runicWords;
     private int nrOfRunicWords;
 
     public Part(int part, String str) {
         System.out.printf("-----====> Part %d <======-------%n%n", part);
         this.words = new ArrayList<>();
         this.inscriptions = new ArrayList<>();
-        this.inscription = "";
-        this.runicWords = new ArrayList<>();
         this.nrOfRunicWords = 0;
-        if (part == 1){
+        if (part == 1 || part == 2){
             String[] datas = str.split("\n\n");
             Matcher header =  Pattern.compile("^WORDS:(.*)$").matcher(datas[0]);
             if (header.find()){
@@ -80,6 +77,76 @@ public class Part implements iPart {
 
     @Override
     public void countPart2() {
+        this.addReverseWords();
+        int totalRunicLetters = 0;
+        for (String inscription: this.inscriptions){
+            // We make a map of the inscription
+            int inscLength = inscription.length();
+            HashMap<Integer, Boolean> inscrMap = new HashMap<>(inscLength);
+            for (int i = 0; i < inscLength; i++){
+                inscrMap.put(i, false);
+            }
+            System.out.println(inscription);
+            int runicWords = 0;
+            for (int i = 0; i < inscLength; i++){
+                for (String word : this.words){
+
+                    // If the word would run past the end, skip this start position
+                    if (i + word.length() > inscLength) continue;
+
+                    boolean matched = true;
+                    for (int j = 0; j < word.length(); j++){
+                        if (inscription.charAt(i + j) != word.charAt(j)) {
+                            matched = false;
+                            break;
+                        }
+                    }
+                    if (matched){
+                        System.out.println("Found " + word);
+                        for (int k = 0; k < word.length(); k++){
+                            inscrMap.put(i + k, true);
+                        }
+                        this.nrOfRunicWords++;
+                        runicWords++;
+                    }
+                }
+            }
+            System.out.printf("Found %d runnic words%n%n", runicWords);
+            int runicLetters = 0;
+            for (boolean b: inscrMap.values()) if (b) runicLetters++;
+            totalRunicLetters += runicLetters;
+            System.out.printf("Found %d runic letters.%n%n%n", runicLetters);
+        }
+        System.out.printf("The total number of runic letters is %d%n", totalRunicLetters);
+    }
+
+    /**
+     * Reverse the given word
+     * @param word A string that represents a word
+     * @return The versed word
+     */
+    private String reverseWord(String word){
+        String reversed = "";
+        int wordL = word.length();
+        if (wordL == 1) return word;
+        for (int i = wordL - 1; i >= 0; i--) {
+            reversed += "" + word.charAt(i);
+        }
+        return reversed;
+    }
+
+    /**
+     * For part2: Add the reversed words to the list of words.
+     */
+    private void addReverseWords(){
+        ArrayList<String> toAdd = new ArrayList<>();
+        for (String word: this.words){
+            String reversedWord = this.reverseWord(word);
+            if(!this.words.contains(reversedWord) && !toAdd.contains(reversedWord)){
+                toAdd.add(reversedWord);
+            }
+        }
+        this.words.addAll(toAdd);
     }
 
     @Override
